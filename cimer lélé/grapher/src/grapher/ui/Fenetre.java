f62de2369b572667cc95d9ea4bde9ac12ffe4114
Robert-Fr <francois-67420@hotmail.fr>
2019-11-08 15:36:09 +0100

    lelelelelele

--- cimer lélé/grapher/src/grapher/ui/Fenetre.java
+++ cimer lélé/grapher/src/grapher/ui/Fenetre.java
@@ -2,6 +2,7 @@ package grapher.ui;
 
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import grapher.fc.FunctionFactory;
 import javafx.application.Application.Parameters;
@@ -10,6 +11,8 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -33,26 +36,14 @@ public class Fenetre extends SplitPane {
 			gc.functions.add(FunctionFactory.createFunction(param));
 		}
 		
-		
+		//on créer tout les composant de la partie gauche de la fenetre 
 		Button plus = new Button(" + ");
 		Button moins = new Button(" - ");
 		ToolBar tb = new ToolBar(plus,moins);
-		EventHandler<ActionEvent> button_handler = new EventHandler<ActionEvent> () {
-			public void handle(ActionEvent e ) {
-				if(e.getSource()==plus) {
-					TextInputDialog dialog= new TextInputDialog();
-				}
-				else {
-					System.out.println("moins");
-				}
-			}
-		};
-		plus.addEventHandler(ActionEvent.ANY, button_handler);
-		moins.addEventHandler(ActionEvent.ANY, button_handler);
-		
 		ListView<String> lv = new ListView<String>();
 		lv.getItems().addAll(params.getRaw());
 		lv.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		
 		lv.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			
 	        @Override
@@ -69,6 +60,37 @@ public class Fenetre extends SplitPane {
 	            gc.bold.clear();
 	        }
 	    });
+		
+		
+		EventHandler<ActionEvent> button_handler = new EventHandler<ActionEvent> () {
+			public void handle(ActionEvent e ) {
+				if(e.getSource()==plus) {
+					TextInputDialog dialog= new TextInputDialog();
+					dialog.setContentText("nouvelle expression : ");
+					dialog.setHeaderText("Expression");
+					dialog.setTitle("Expression");
+					Optional<String> result= dialog.showAndWait();
+					if(!result.isPresent()) {return; }
+					String expression= result.get();
+					try {
+						gc.functions.add(FunctionFactory.createFunction(expression));
+						lv.getItems().add(expression);
+						gc.redraw();
+					} catch (RuntimeException re) {
+						Alert alert = new Alert(AlertType.ERROR,"\"" + expression +"\"\n" + "Expression non valide !");
+						alert.showAndWait();
+					}
+				}
+				else {
+					//supprimer la fonction : récupérer l'indice dasn la liste view si y'en a une de sélectionnée, supprimer et supprimer du gc
+					System.out.println("moins");
+				}
+			}
+		};
+		plus.addEventHandler(ActionEvent.ANY, button_handler);
+		moins.addEventHandler(ActionEvent.ANY, button_handler);
+		
+		
 		
 		bp.setCenter(lv);
 		bp.setBottom(tb);
